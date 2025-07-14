c7243a445b68714af45b00582ba84941d68ebc3d
oC10H15No <Shu_12138@126.com>
2025-07-14 21:48:47 +0800

    Solution for lab/cow

--- kernel/riscv.h
+++ kernel/riscv.h
@@ -17,8 +17,6 @@ r_mhartid()
 #define MSTATUS_MPP_U (0L << 11)
 #define MSTATUS_MIE (1L << 3)    // machine-mode interrupt enable.
 
-#define PTE_COW (1L << 8)
-
 static inline uint64
 r_mstatus()
 {
@@ -364,6 +362,7 @@ typedef uint64 *pagetable_t; // 512 PTEs
 #define PTE_W (1L << 2)
 #define PTE_X (1L << 3)
 #define PTE_U (1L << 4) // user can access
+#define PTE_COW (1L << 8)
 
 // shift a physical address to the right place for a PTE.
 #define PA2PTE(pa) ((((uint64)pa) >> 12) << 10)
